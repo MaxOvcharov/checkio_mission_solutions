b7f0f00b5963733a66016ca78cb3ba682f7aa65e
MaxOvcharov <ovcharovmax@yandex.ru>
2018-01-13 23:39:31 +0300

    Mission 'The Most Wanted Letter' is completed

---
+++ home_missions/the_most_wanted_letter.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+"""
+Дан текст, который содержит различные английские буквы и знаки препинания.
+Вам необходимо найти самую частую букву в тексте. Результатом должна быть
+буква в нижнем регистре.
+
+При поиске самой частой буквы, регистр не имеет значения, так что при
+подсчете считайте, что "A" == "a". Убедитесь, что вы не считайте знаки
+препинания, цифры и пробелы, а только буквы.
+
+Если в тексте две и больше буквы с одинаковой частотой, тогда результатом
+будет буква, которая идет первой в алфавите. Для примера, "one"
+содержит "o", "n", "e" по одному разу, так что мы выбираем "e".
+
+Предусловия:
+text содержит только ASCII символы.
+0 < len(text) ≤ 105
+
+Вх. данные: Текст для анализа, как строка.
+
+Вых. данные: Наиболее частая буква, как строка.
+"""
+import string
+
+
+# def checkio(text):
+#     a = list(text.lower())
+#     a.sort()
+#     count = [0, 0]
+#     for i in a:
+#         if i.isalpha():
+#             temp = a.count(i)
+#             if temp > count[0]:
+#                 count[0] = temp
+#                 count[1] = i
+#     return count[1]
+
+
+# def checkio_best_solution(text):
+def checkio(text):
+    """
+    We iterate through latyn alphabet and count each letter in the text.
+    Then 'max' selects the most frequent letter.
+    For the case when we have several equal letter,
+    'max' selects the first from they.
+    """
+    text = text.lower()
+    return max(string.ascii_lowercase, key=text.count)
+
+
+if __name__ == '__main__':
+    #  These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(u"Hello World!") == "l", "Hello test"
+    assert checkio(u"How do you do?") == "o", "O is most wanted"
+    assert checkio(u"One") == "e", "All letter only once."
+    assert checkio(u"Oops!") == "o", "Don't forget about lower case."
+    assert checkio(u"AAaooo!!!!") == "a", "Only letters."
+    assert checkio(u"abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio(u"a" * 9000 + u"b" * 1000) == "a", "Long."
+    print("The local tests are done.")
