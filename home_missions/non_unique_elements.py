a9f663fc51c16b5ab61243c27420dffb3f762ec5
MaxOvcharov <ovcharovmax@yandex.ru>
2018-01-14 23:19:21 +0300

    Mission 'Non-unique Elements' is completed

---
+++ home_missions/non_unique_elements.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+"""
+Дан непустой массив целых чисел (X). В этой задаче вам нужно
+вернуть массив, состоящий только из неуникальных элементов данного массива.
+
+Для этого необходимо удалить все уникальные элементы (которые присутствуют
+в данном массиве только один раз). Для решения этой задачи не меняйте
+оригинальный порядок элементов. Пример: [1, 2, 3, 1, 3], где 1 и 3
+неуникальные элементы и результат будет [1, 3, 1, 3].
+
+Предусловия:
+0 < len(data) < 1000
+
+Вх. данные: Список (list) целых чисел (int).
+
+Вых. данные: Список (list) целых чисел (int).
+"""
+
+
+def checkio_my(data):
+    return [i for i in data if data.count(i) > 1]
+
+
+def checkio_best_solution(data):
+    return [i for i in data if data.count(i) > 1]
+
+
+if __name__ == "__main__":
+    #  These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio_my([1]), list), "The result must be a list"
+    assert checkio_my([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio_my([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio_my([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio_my([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
--- home_missions/the_most_wanted_letter.py
+++ home_missions/the_most_wanted_letter.py
@@ -23,7 +23,7 @@ text содержит только ASCII символы.
 import string
 
 
-def checkio(text):
+def checkio_my(text):
     a = list(text.lower())
     a.sort()
     count = [0, 0]
@@ -49,12 +49,12 @@ def checkio_best_solution(text):
 
 if __name__ == '__main__':
     #  These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio(u"Hello World!") == "l", "Hello test"
-    assert checkio(u"How do you do?") == "o", "O is most wanted"
-    assert checkio(u"One") == "e", "All letter only once."
-    assert checkio(u"Oops!") == "o", "Don't forget about lower case."
-    assert checkio(u"AAaooo!!!!") == "a", "Only letters."
-    assert checkio(u"abe") == "a", "The First."
+    assert checkio_my(u"Hello World!") == "l", "Hello test"
+    assert checkio_my(u"How do you do?") == "o", "O is most wanted"
+    assert checkio_my(u"One") == "e", "All letter only once."
+    assert checkio_my(u"Oops!") == "o", "Don't forget about lower case."
+    assert checkio_my(u"AAaooo!!!!") == "a", "Only letters."
+    assert checkio_my(u"abe") == "a", "The First."
     print("Start the long test")
-    assert checkio(u"a" * 9000 + u"b" * 1000) == "a", "Long."
+    assert checkio_my(u"a" * 9000 + u"b" * 1000) == "a", "Long."
     print("The local tests are done.")
